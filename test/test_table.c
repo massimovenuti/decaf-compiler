e4792eaefe2aa5e23831944449bc754bca4f8bb5
Rayan LAJARGE <rlajarge@etu.unistra.fr>
2021-11-21 14:35:39 +0100

    test refactoring

--- test/test_table.c
+++ test/test_table.c
@@ -2,118 +2,125 @@
 
 int main(int argc, char **argv)
 {
-    if (argc != 2)
-    {
-        fprintf(stderr, "invalid arguments\n");
-        exit(EXIT_FAILURE);
-    }
+    (void)argc;
+    (void)argv;
 
     Tos table = new_tos();
+    int errors = 0;
     
-    int test = atoi(argv[1]);
-    switch (test)
+    // test 1 : 1 variable, 1 scope
+    table = push_tos(table);
+
+    table = tos_newname(table, "compteur", S_INT, 0);
+    errors += (tos_lookup(table, "compteur") == NULL) ? 1 : 0;
+
+    table = pop_tos(table);
+
+    printf("Test 1 : ");
+    if (table == NULL && !errors)  
     {
-    case 1: // 1 variable, 1 scope
-        table = push_tos(table);
-        
-        table = tos_newname(table, "compteur", S_INT, 0);
-        print_tos(table);
-        
-        table = pop_tos(table);
-        printf("\n");
-
-        if (table == NULL) 
-        {
-            printf("[ok]\n");
-        }
-        else
-        {
-            printf("[ko]\n");
-            exit(EXIT_FAILURE);
-        }
-        break;
-    case 2: // variable redefinition, 1 scope
-        table = push_tos(table);
-        
-        table = tos_newname(table, "compteur", S_INT, 0);
-        print_tos(table);
-        printf("\n");
-        
-        table = tos_newname(table, "compteur", S_INT, 0);
-        // error expected here ...
-        
-        if (table == NULL) 
-        {
-            printf("[ok]\n");
-        }
-        else
-        {
-            printf("[ko]\n");
-            exit(EXIT_FAILURE);
-        }
-        break;
-    case 3: // sereval variables, 2 scope
-        table = push_tos(table);
-        
-        table = tos_newname(table, "compteur", S_INT, 0);
-        table = tos_newname(table, "diviseur", S_INT, 0);
-        table = tos_newname(table, "test", S_BOOL, 0);
-        print_tos(table);
-        printf("\n");
-        
-        table = push_tos(table);
-        
-        table = tos_newname(table, "test", S_BOOL, 0);
-        table = tos_newname(table, "symbole", S_INT, 0);
-        print_tos(table);
-        printf("\n");
-        
-        table = pop_tos(table);
-        table = pop_tos(table);
-
-        if (table == NULL) 
-        {
-            printf("[ok]\n");
-        }
-        else
-        {
-            printf("[ko]\n");
-            exit(EXIT_FAILURE);
-        }
-        break;
-    case 4: // sereval variables, 2 scope with redefinition
-        table = push_tos(table);
-        
-        table = tos_newname(table, "compteur", S_INT, 0);
-        table = tos_newname(table, "diviseur", S_INT, 0);
-        table = tos_newname(table, "test", S_BOOL, 0);
-        print_tos(table);
-        printf("\n");
-        
-        table = push_tos(table);
-        
-        table = tos_newname(table, "test", S_BOOL, 0);
-        table = tos_newname(table, "nombre", S_INT, 0);
-        print_tos(table);
-        printf("\n");
-        
-        table = tos_newname(table, "test", S_BOOL, 0);
-        // error expected here ...
-
-        if (table == NULL) 
-        {
-            printf("[ok]\n");
-        }
-        else
-        {
-            printf("[ko]\n");
-            exit(EXIT_FAILURE);
-        }
-        break;
-    default:
-        printf("unknown test\n");
-        break;
+        printf("[ok]\n");
     }
+    else
+    {
+        printf("[ko]\n");
+        exit(EXIT_FAILURE);
+    }
+    errors = 0;
+
+    // test 2 : 1 variable with redefinition, 1 scope
+    table = push_tos(table);
+    
+    table = tos_newname(table, "compteur", S_INT, 0);
+    
+    errors += (tos_lookup(table, "compteur") == NULL) ? 1 : 0;
+    
+    // error expected here ...
+    printf("=> error expected here ...\n");
+    table = tos_newname(table, "compteur", S_INT, 0);
+    
+    
+    printf("Test 2 : ");
+    if (table == NULL && !errors) 
+    {
+        printf("[ok]\n");
+    }
+    else
+    {
+        printf("[ko]\n");
+        exit(EXIT_FAILURE);
+    }
+    errors = 0;
+
+    // test 3 : several variables, 2 scopes
+    table = push_tos(table);
+    
+    table = tos_newname(table, "compteur", S_INT, 0);
+    table = tos_newname(table, "diviseur", S_INT, 0);
+    table = tos_newname(table, "test", S_BOOL, 0);
+
+    errors += (tos_lookup(table, "compteur") == NULL) ? 1 : 0;
+    errors += (tos_lookup(table, "diviseur") == NULL) ? 1 : 0;
+    errors += (tos_lookup(table, "test") == NULL) ? 1 : 0;
+    
+    table = push_tos(table);
+    
+    table = tos_newname(table, "test", S_BOOL, 0);
+    table = tos_newname(table, "nombre", S_INT, 0);
+    
+    errors += (tos_lookup(table, "test") == NULL) ? 1 : 0;
+    errors += (tos_lookup(table, "nombre") == NULL) ? 1 : 0;
+    
+    table = pop_tos(table);
+    table = pop_tos(table);
+    
+    printf("Test 3 : ");
+    if (table == NULL && !errors) 
+    {
+        printf("[ok]\n");
+    }
+    else
+    {
+        printf("[ko]\n");
+        exit(EXIT_FAILURE);
+    }
+    errors = 0;
+
+    // test 4 : several variables with redefinition, 2 scopes
+    table = push_tos(table);
+    
+    table = tos_newname(table, "compteur", S_INT, 0);
+    table = tos_newname(table, "diviseur", S_INT, 0);
+    table = tos_newname(table, "test", S_BOOL, 0);
+
+
+    errors += (tos_lookup(table, "compteur") == NULL) ? 1 : 0;
+    errors += (tos_lookup(table, "diviseur") == NULL) ? 1 : 0;
+    errors += (tos_lookup(table, "test") == NULL) ? 1 : 0;
+    
+    table = push_tos(table);
+    
+    table = tos_newname(table, "test", S_BOOL, 0);
+    table = tos_newname(table, "nombre", S_INT, 0);
+
+    errors += (tos_lookup(table, "test") == NULL) ? 1 : 0;
+    errors += (tos_lookup(table, "nombre") == NULL) ? 1 : 0;
+    
+    // error expected here ...
+    printf("=> error expected here ...\n");
+    table = tos_newname(table, "test", S_BOOL, 0);
+    
+    printf("Test 4 : ");
+    if (table == NULL && !errors) 
+    {
+        printf("[ok]\n");
+    }
+    else
+    {
+        printf("[ko]\n");
+        exit(EXIT_FAILURE);
+    }
+    errors = 0;
 
     return EXIT_SUCCESS;
 }
