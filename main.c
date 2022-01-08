b216d86e28b9af81ec9185ec6ab8b8878af2f52e
ULUCAN AHMET-SEFA <ahmet-sefa.ulucan@etu.unistra.fr>
2022-01-08 18:59:59 +0100

    Debut des options

--- main.c
+++ main.c
@@ -8,39 +8,53 @@ extern int yydebug;
 
 const char *inname = NULL;
 
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
     char *outname = "a.mips";
 
     int c;
-    while (1)
-    {
-        c = getopt(argc, (char *const *)argv, "o:");
-        if (c == -1)
-            break;
+    int option_index = 0;
+    int print_global = 0;
+    int print_tos = 0;
+    static struct option long_options[] = {
+        {"o", required_argument, 0, 0},
+        {"tos", no_argument, 0, 't'},
+        {"version", no_argument, 0, 'v'},
+        {"quad", no_argument, 0, 'q'},
+        {0, 0, 0, 0}};
 
+    while ((c = getopt_long_only(argc, argv, "o:tv", long_options, &option_index)) != -1)
+    {
         switch (c)
         {
+
+        case 'v':
+            printf("Membres du projet :\n");
+            printf("Rayan LAJARGE\nAhmet Sefa ULUCAN\n");
+            printf("Massimo VENUTI\nAlexandre VOGEL\n");
+            return EXIT_SUCCESS;
+
+        case 't':
+            print_tos  = 1;
+            break;
 
         case 'o':
             outname = optarg;
             break;
 
+        case 'q':
+            print_global = 1;
+            break;
+
         default:
-            fprintf(stderr, "Error option\n");
+            break;
         }
     }
 
-    for (int i = optind; i < argc; i++)
+    if (optind < argc)
     {
-        if (inname == NULL)
-        {
-            inname = argv[i];
-        }
-        else
-        {
-            fprintf(stderr, "Error input\n");
-        }
+        while (optind < argc)
+            inname = argv[optind++];
     }
 
     FILE *input = freopen(inname, "r", stdin);
@@ -50,7 +64,10 @@ int main(int argc, char const *argv[])
     {
         exit(EXIT_FAILURE);
     }
-    print_globalcode();
+    if (print_global)
+    {
+        print_globalcode();
+    }
     FILE *output = fopen(outname, "w");
     gen_mips(globalcode, nextquad, output);
     freecode();
